3c6c092de87f35fcff9311a6c99fb716a366608e
lpantojaa <lmpantoja@hotmail.com>
2023-07-07 16:51:23 +0000

    Setting table miss flow rule to new switches

--- ryuapp.py
+++ ryuapp.py
@@ -1,5 +1,8 @@
 from ryu.base import app_manager
+from ryu.controller import ofp_event
 from ryu.ofproto import ofproto_v1_3
+from ryu.controller.handler import CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
 import os
 
 K_MINIMUM = 4
@@ -21,3 +24,18 @@ class FatTreeRyuApp(app_manager.RyuApp):
         instruction = [datapath.ofproto_parser.OFPInstructionActions(datapath.ofproto.OFPIT_APPLY_ACTIONS, actions)]
         mod_msg = datapath.ofproto_parser.OFPFlowMod(datapath = datapath, priority = priority, match = match, instructions = instruction)
         datapath.send_msg(mod_msg)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def packet_features_handler(self, ev):
+        datapath = ev.msg.datapath  # Extract the datapath object from the event
+        priority = 0  # Define the priority for our flow rule
+        k = self.k 
+
+        # Create a match object with no specific match fields (i.e., it will match all packets)
+        match = datapath.ofproto_parser.OFPMatch()
+        
+        # Create an action that outputs all packets to the controller
+        actions = [datapath.ofproto_parser.OFPActionOutput(datapath.ofproto.OFPP_CONTROLLER, datapath.ofproto.OFPCML_NO_BUFFER)]
+        
+        # Add a flow entry to the switch that matches all packets and sends them to the controller
+        self.add_flow(datapath, priority, match, actions)
